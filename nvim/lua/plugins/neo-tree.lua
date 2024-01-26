72777e784467fc481c12a3157d9f3cb761cd2177
kevst-dev <kevst-dev@protonmail.com>
2024-01-26 09:36:40 -0500

    feat: Agregando y configurando pluging Vista.vim

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,21 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v2.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>T", ":Neotree toggle<cr>" },
+      { "<leader>t", ":Neotree <cr>" },
+    },
   },
-  keys = {
-    { "<leader>T", ":Neotree toggle<cr>" },
-    { "<leader>t", ":Neotree <cr>" },
-  },
+  {
+    "liuchengxu/vista.vim",
+    keys = {
+      { "<leader>b", ":Vista<cr>" },
+    }
+  }
 }
