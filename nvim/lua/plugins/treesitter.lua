11a980d05ef62a51437902b80c682e61a87698b0
kevst-dev <kevst-dev@protonmail.com>
2024-01-26 12:24:12 -0500

    fix(justfile): solucionando bug con el plugin que da syntaxis de color a "justfile"

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -67,16 +67,7 @@ return {
     },
   },
   {
-    "IndianBoy42/tree-sitter-just",
-    config = function()
-      require("nvim-treesitter.parsers").get_parser_configs().just = {
-        install_info = {
-          url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-          files = { "src/parser.c", "src/scanner.cc" },
-          branch = "main",
-        },
-        maintainers = { "@IndianBoy42" },
-      }
-    end,
-  },
+    "NoahTheDuke/vim-just",
+    ft = { "just" },
+  }
 }
