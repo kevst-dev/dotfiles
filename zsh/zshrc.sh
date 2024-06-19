a4af88b493e0984f3e036519981d7144bdc982d8
kevst-dev <kevst-dev@protonmail.com>
2024-06-19 11:03:03 -0500

    refactor(zsh): eliminando la configuración de un fzf, no lo estaba utilizando

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -8,10 +8,10 @@ main() {
   export ZSH_CONFIG_PATH="$HOME"/.dotfiles/zsh
 
   source "$ZSH_CONFIG_PATH/src/history.sh"
+
   source "$ZSH_CONFIG_PATH/src/plugins.sh"
   source "$ZSH_CONFIG_PATH/src/prompt.sh"
   source "$ZSH_CONFIG_PATH/src/commands.sh"
-  source "$ZSH_CONFIG_PATH/src/fuzzy_finder.sh"
 
   # neofetch
 }
