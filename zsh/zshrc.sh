78b1456c0836944032e7c5f510c61422403cef01
kevst-dev <kevst-dev@protonmail.com>
2024-06-19 10:56:03 -0500

    refeactor(zsh): eliminando el archivo 'dirs.sh' no era relevante

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -2,11 +2,12 @@
 
 main() {
 
+  export PATH="$HOME/.local/bin:$PATH"
+
   # Definiendo el directorio de configuración
   export ZSH_CONFIG_PATH="$HOME"/.dotfiles/zsh
 
   source "$ZSH_CONFIG_PATH/src/history.sh"
-  source "$ZSH_CONFIG_PATH/src/dirs.sh"
   source "$ZSH_CONFIG_PATH/src/plugins.sh"
   source "$ZSH_CONFIG_PATH/src/prompt.sh"
   source "$ZSH_CONFIG_PATH/src/commands.sh"
