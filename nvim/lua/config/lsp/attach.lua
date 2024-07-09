1679a753338c9d061cbdbf89f88b8df462531a91
kevst-dev <kevst-dev@protonmail.com>
2024-07-09 16:03:44 -0500

    feat: agregando configuracion de folding

--- nvim/lua/config/lsp/attach.lua
+++ nvim/lua/config/lsp/attach.lua
@@ -1,23 +1,25 @@
+local filetype_attach = setmetatable({}, {
+  __index = function()
+    return function() end
+  end,
+})
+
 return function(client, bufnr)
-  local _ = vim.api.nvim_buf_get_option(0, "filetype")
+  local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+
   -- keymaps for lsp
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "LSP Help information of symbol under the cursor" })
-  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = 0, desc = "LSP Rename symbol under cursor" })
-  vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, { buffer = 0, desc = "LSP Signature help" })
-  vim.keymap.set("n", "<leader>vo", ":LspRestart<cr>", { noremap = true, desc = "LSP Restart Server" })
-  vim.keymap.set({ "n", "v" }, "<leader>vca", vim.lsp.buf.code_action, { buffer = 0, desc = "LSP Code actions" })
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0, desc = "LSP: Mostrar ayuda" })
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { buffer = 0, })
+  vim.keymap.set("i", "<c-h>", vim.lsp.buf.signature_help, { buffer = 0 })
+  vim.keymap.set("n", "<leader>vo", ":LspRestart<cr>", { noremap = true })
+  vim.keymap.set({ "n", "v" }, "<leader>vca", vim.lsp.buf.code_action, {})
   vim.keymap.set("n", "<leader>vf", function()
-    return vim.lsp.buf.format {
-      async = true,
-      filter = function(cli)
-        return cli.name ~= "lua_ls"
-      end,
-    }
-  end, { buffer = 0, desc = "LSP format file" })
+    return vim.lsp.buf.format({ async = true })
+  end, {})
+
 
   vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
-  if client.server_capabilities.inlayHintProvider then
-    vim.lsp.inlay_hint(bufnr, true)
-  end
+  -- Attach any filetype specific options to the client
+  filetype_attach[filetype](client, bufnr)
 end
